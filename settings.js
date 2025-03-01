1ce23a85d4f9443c309ac90083700b105e648cc8
Hsapex <marvelsrael54@gmail.com>
2025-03-01 13:42:29 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,16 +3,16 @@ const chalk = require('chalk')
 
 
 //contact details
-global.ownernomer = "2348109263390"
-global.ownername = "ElitePro"
-global.ytname = "YT: @ElitePro-Empire"
-global.socialm = "GitHub: Elite-MD"
-global.location = "Nigeria, Port Harcourt"
+global.ownernomer = "2348109547767"
+global.ownername = "ᴀᴘᴇxㅤ  𓃵☠"
+global.ytname = "YT: "
+global.socialm = "GitHub: Apex"
+global.location = "Nigeria, Lagos"
 
 
-global.ownernumber = '2348109263390'  //creator number
-global.ownername = 'ElitePro' //owner name
-global.botname = 'ELITE-PRO-V1' //name of the bot
+global.ownernumber = '2348109547767'  //creator number
+global.ownername = 'Apex' //owner name
+global.botname = 'ᴀᴘᴇxㅤ  𓃵☠' //name of the bot
 
 
 //custom prefix
@@ -22,18 +22,18 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 
 
 global.autoread = false //auto read messages
-global.autobio = true //auto update bio
+global.autobio = false//auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
 
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'Elite-Pro\n\nContact: +2348109263390'
+global.author = 'Elite-Pro\n\nContact: +2348109547767'
 
 //set your menu picture
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg'
